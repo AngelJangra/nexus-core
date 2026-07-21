11117bdb2c99c852c28bdf7d40661b95a5264c27
AngelJangra <jangraangel39@gmail.com>
2026-07-22 00:32:32 +0530

    Delete api directory

--- api/logout.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res) => {
-    res.setHeader('Set-Cookie', 'dashboard_auth=; Max-Age=0; Path=/; HttpOnly; SameSite=Lax');
-    res.setHeader('Location', '/api/dashboard');
-    res.status(302).send();
-};
